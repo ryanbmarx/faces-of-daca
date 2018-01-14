a9c46842f82bbb33e74088a1d1e5618dfd8fa335
ryanbmarx <ryanbmarx@gmail.com>
2018-01-13 18:45:07 -0600

    audio elements will pause and reset when clicked twice

--- js/src/app.js
+++ js/src/app.js
@@ -28,21 +28,35 @@ window.addEventListener('DOMContentLoaded', function(e){
 	const audioButtons = [].slice.call(document.querySelectorAll('.profile__quote-icon'));
 	audioButtons.forEach(a => {
 		a.addEventListener('click', function(e){
-			
-			// Toggle active state for button
-			if (document.querySelector('.profile__quote-icon.profile__quote-icon--active') != null) {
-				document.querySelector('.profile__quote-icon.profile__quote-icon--active').classList.remove('profile__quote-icon--active');
-			}
-			this.classList.add('profile__quote-icon--active');
-
+			// The currently selected audio element
 			const audioEl = this.parentNode.querySelector('.profile__quote-audio');
 
-			// Stop any/all playing audios
-			const audioEls = [].slice.call(document.querySelectorAll('.profile__quote-audio'));
-			audioEls.forEach(audio => audio.pause());
-			
-			// play the one the user chose
-			audioEl.play();
+			if (!audioEl.paused){
+				// If the button clicked is a playing element, then just stop it.
+				audioEl.pause();
+				audioEl.currentTime = 0;
+				this.classList.remove('profile__quote-icon--active');
+
+			} else {
+				// If we have clicked on a new button ...
+
+				// Toggle active state for button
+				if (document.querySelector('.profile__quote-icon.profile__quote-icon--active') != null) {
+					document.querySelector('.profile__quote-icon.profile__quote-icon--active').classList.remove('profile__quote-icon--active');
+				}
+				this.classList.add('profile__quote-icon--active');
+
+
+				// Stop any/all playing audios
+				const audioEls = [].slice.call(document.querySelectorAll('.profile__quote-audio'));
+				audioEls.forEach(audio => {
+					audio.pause();
+					audioEl.currentTime = 0;
+				});
+				
+				// play the one the user chose
+				audioEl.play();
+			}
 		})
 	})
 
