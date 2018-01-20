2148a407d4ed6a35a4425279842c39d7e1293ce0
ryanbmarx <ryanbmarx@gmail.com>
2018-01-20 17:09:18 -0600

    add some helpful, insightful comments

--- js/src/app.js
+++ js/src/app.js
@@ -2,15 +2,13 @@ const inView = require('in-view');
 
 
 window.addEventListener('DOMContentLoaded', function(e){
-
+	
+	// After 45 seconds, stop the video
 	const videoTimer = setTimeout(function(){
-		// After 30 seconds, stop the video
-		console.log('stop');
 		document.querySelector('.intro__video video').pause();
 	}, 45000)
 
 	// lazyloading the images
-
 	let imageSize = "";
 	if (window.innerWidth > 800){
 		imageSize = "desktop";
@@ -30,23 +28,25 @@ window.addEventListener('DOMContentLoaded', function(e){
             el.classList.remove('profile__image--blur');
         });
 
+    // power the audio buttons
 	const audioButtons = [].slice.call(document.querySelectorAll('.profile__quote-icon'));
 	audioButtons.forEach(a => {
 		a.addEventListener('click', function(e){
-			// The currently selected audio element
+
+			// Grab the currently selected audio element
 			const audioEl = this.parentNode.querySelector('.profile__quote-audio');
 
 			if (!audioEl.paused){
-				// If the button clicked is a playing element, then just stop it.
+				// If the button clicked is a playing element, then just stop the audio.
 				audioEl.pause();
-				audioEl.currentTime = 0;
+				audioEl.currentTime = 0; // This puts it back to the beginning so it will start over if played again.
 				this.classList.remove('profile__quote-icon--active');
 
 			} else {
 				// If we have clicked on a new button ...
 
 				// Toggle active state for button
-				if (document.querySelector('.profile__quote-icon.profile__quote-icon--active') != null) {
+				if (document.querySelector('.profile__quote-icon.profile__quote-icon--active') !== null) {
 					document.querySelector('.profile__quote-icon.profile__quote-icon--active').classList.remove('profile__quote-icon--active');
 				}
 				this.classList.add('profile__quote-icon--active');
